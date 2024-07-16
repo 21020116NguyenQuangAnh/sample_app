57217f73e408a8c5315e0d2cbf38833cc55a0e39
21020116NguyenQuangAnh <21020116@vnu.edu.vn>
2024-07-16 12:19:55 +0700

    Chapter 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   PERMITTED_ATTRIBUTES = %i(name email password password_confirmation).freeze
+  RESET_PASSWORD_ATTRIBUTES = %i(password password_confirmation).freeze
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   validates :name, presence: true, length: {maximum: Settings.max_name_length}
   validates :email, presence: true,
@@ -58,6 +59,20 @@ class User < ApplicationRecord
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.hours_expired.hours.ago
   end
 
   private
